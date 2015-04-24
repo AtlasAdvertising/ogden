5053415844b7c90e937648990ec90fbdb0204fd7
chrisdatlas <christopherd@atlas-advertising.com>
2015-04-24 09:36:57 -0600

    Changes to navigation

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,16 @@
 var gulp = require('gulp');
 
 // Include Plugins
-var ftp = require('vinyl-ftp');
+var ftp = require('gulp-ftp');
 var sass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 
 // Compile Sass
 gulp.task('sass', function() {
-  return sass('scss/main.scss', { sourcemap: true })
+  return gulp.src('scss/main.scss')
     .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write())
+    .pipe(sass({style: 'compressed'}))
     .pipe(gulp.dest('css'));
 });
 
@@ -25,50 +24,81 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('js/'))
 });
 
-// Dev FTP: Sass and CSS
-gulp.task('ftp-dev', function() {
-
-    var connDev = ftp.create({
+// Dev FTP: CSS
+gulp.task('ftp-dev-css', function() {
+  return gulp.src('css/*')
+    .pipe(ftp({
       host: '66.241.194.6',
       user: 'chrisd',
       pass: 'aachrisd',
-    });
-
-    var globsDev = [
-      'css/**',
-      'scss/**'
-    ]
-
-    return gulp.src( globsDev, { base: '.', buffer: false } )
-        .pipe( connDev.newer( '/ogden' ) ) // only upload newer files 
-        .pipe( connDev.dest( '/ogden' ) );
-
+      remotePath: 'ogden/css'
+    }));
 });
 
-// Prod FTP: Sass and CSS
-gulp.task('ftp-prod', function() {
+// Dev FTP: Sass
+gulp.task('ftp-dev-scss', function() {
+  return gulp.src('scss/*')
+    .pipe(ftp({
+      host: '66.241.194.6',
+      user: 'chrisd',
+      pass: 'aachrisd',
+      remotePath: 'ogden/scss'
+    }));
+});
 
-    var connProd = ftp.create({
+// Dev FTP: Scripts
+gulp.task('ftp-dev-scripts', function() {
+  return gulp.src('js/*')
+    .pipe(ftp({
+      host: '66.241.194.6',
+      user: 'chrisd',
+      pass: 'aachrisd',
+      remotePath: 'ogden/js'
+    }));
+});
+
+// Prod FTP: CSS
+gulp.task('ftp-css', function() {
+  return gulp.src('css/*')
+    .pipe(ftp({
       host: '66.241.194.5',
       user: 'chrisd',
       pass: 'G00berz',
-    });
-
-    var globsProd = [
-      'css/**',
-      'scss/**'
-    ]
-
-    return gulp.src( globsProd, { base: '.', buffer: false } )
-        .pipe( connProd.newer( '/Atlas/Sites/KenticoCMS 7.0.103/ogden' ) ) // only upload newer files 
-        .pipe( connProd.dest( '/Atlas/Sites/KenticoCMS 7.0.103/ogden' ) );
-
+      remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/ogden/css'
+    }));
 });
+
+// Prod FTP: Sass
+gulp.task('ftp-scss', function() {
+  return gulp.src('scss/*')
+    .pipe(ftp({
+      host: '66.241.194.5',
+      user: 'chrisd',
+      pass: 'G00berz',
+      remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/ogden/scss'
+    }));
+});
+
+// Prod FTP: Scripts
+gulp.task('ftp-scripts', function() {
+  return gulp.src('js/*')
+    .pipe(ftp({
+      host: '66.241.194.6',
+      user: 'chrisd',
+      pass: 'aachrisd',
+      remotePath: '/Atlas/Sites/KenticoCMS 7.0.103/ogden/js'
+    }));
+});
+
+// FTP Everything to Dev and Prod
+gulp.task('ftp-all', ['ftp-dev-css', 'ftp-dev-scss', 'ftp-dev-scripts', 'ftp-css', 'ftp-scss', 'ftp-scripts']);
 
 // Watch Files For Changes
 gulp.task('watch', function() {
   gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['ftp-dev','ftp-prod']);
+  gulp.watch('css/*.css', ['ftp-dev-css', 'ftp-css']);
+  gulp.watch('scripts/*.js', ['scripts']);
+  gulp.watch('js/*.js', ['ftp-dev-sripts'])
 });
 
 // Default Task
